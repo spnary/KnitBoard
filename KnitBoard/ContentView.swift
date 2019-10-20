514c9dfb5171205314d0a9cd2e3bdebcca6686f4
Stephen Nary <spnary@gmail.com>
2019-10-20 12:41:04 -0400

    Initial Commit

---
+++ KnitBoard/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  KnitBoard
+//
+//  Created by Stephen Nary on 10/20/19.
+//  Copyright © 2019 Stephen Nary. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
